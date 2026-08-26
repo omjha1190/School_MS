1345b20c79a928ed2ebab00332830e609a4d9435
Om Jha <omjha1190@gmail.com>
2026-08-26 10:36:40 +0530

    Staff Crud Functionality done

--- schoolmanagementsystem/staffs/urls.py
+++ schoolmanagementsystem/staffs/urls.py
@@ -4,4 +4,6 @@ from staffs import views
 urlpatterns = [
     path("insert/", views.insert_staff, name="insert_staff"),
     path("manage/", views.manage_staffs, name="manage_staffs"),
+    path("edit/<int:id>/", views.edit_staff, name="edit_staff"),
+    path("delete/<int:id>/", views.delete_staff, name="delete_staff"),
 ]
--- schoolmanagementsystem/staffs/views.py
+++ schoolmanagementsystem/staffs/views.py
@@ -28,3 +28,35 @@ def manage_staffs(req):
         "staffs" : Staff.objects.all(),
     }
     return render(req, "staff/manage_staffs.html", data)
+
+def edit_staff(req, id):
+    staff = Staff.objects.get(id=id)
+    data ={
+        "staff" : staff,
+        "staff_types": Staff.STAFF_TYPES,
+        "gender_choices": Staff.GENDER_CHOICES,
+    }
+    if req.method == "POST":
+
+        staff.name = req.POST.get('name')
+        staff.cover_image = req.FILES.get('cover_image')
+        staff.address = req.POST.get('address')
+        staff.staff_type = req.POST.get('staff_type')
+        staff.gender = req.POST.get('gender')
+        staff.phone = req.POST.get('phone')
+        staff.email = req.POST.get('email') or None
+        staff.joining_date = req.POST.get('joining_date')
+        staff.salary = req.POST.get('salary')
+        staff.save()
+        return redirect("manage_staffs")
+    return render(req, "staff/insert_staff.html", data)
+
+def delete_staff(req, id):
+    data = {}
+    try :
+        staff = Staff.objects.get(id=id)
+        staff.delete()
+        return redirect("manage_staffs")
+    except Staff.DoesNotExist:
+        data ['error'] = "This Staff does not exist"
+    return redirect("manage_staffs")
